a364ead08fce91104b7ff6cf07cf59f1f16087e3
Isaac Rico <isaacarnell.rico@gmail.com>
2024-01-17 20:27:56 -0800

    Fixed Views Error

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));

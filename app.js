9e0091043f3fd27deff0c932018941d42f58eb6e
Isaac Rico <isaacarnell.rico@gmail.com>
2024-01-17 20:42:32 -0800

    Fixed CSS error

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
 
 
 // Function to load and parse CSV data
